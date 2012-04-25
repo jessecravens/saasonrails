e9c5409fe9ae4af7b8457183144f073e9f1c782f
Wahaj Ali <wahaj.ali@7vals.com>
2012-04-25 16:48:13 +0500

    Added twitter bootstrap UI, devise, cancan and rolify

---
+++ app/models/profile.rb
@@ -0,0 +1,13 @@
+class Profile
+  include Mongoid::Document
+  field :first_name
+  field :last_name
+
+  belongs_to :user
+
+  attr_accessible :first_name, :last_name
+
+  def full_name 
+    "#{first_name} #{last_name}"
+  end
+end
