a1cd9a6d77372e5a00bf1f875e330302adbe887b
Shirjeel Alam <shirjeel20@gmail.com>
2012-04-26 23:25:45 +0500

    Paperclip for avatar uploading

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,14 @@
 class Profile
   include Mongoid::Document
+  include Mongoid::Paperclip
   field :first_name
   field :last_name
 
   belongs_to :user
 
-  attr_accessible :first_name, :last_name
+  has_mongoid_attached_file :avatar, styles: { thumb: '180x180' }
+
+  attr_accessible :first_name, :last_name, :avatar
   validates_presence_of :first_name, :last_name
 
   def full_name 
