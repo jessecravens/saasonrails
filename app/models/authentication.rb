9efa21e5e91513ff58bc2f2db7961d01ec658f99
Shirjeel Alam <shirjeel20@gmail.com>
2012-04-30 00:54:47 +0500

    Sign up with facebook

---
+++ app/models/authentication.rb
@@ -0,0 +1,13 @@
+class Authentication
+  include Mongoid::Document
+  field :provider, type: String
+  field :uid, type: String
+  field :access_token, type: String
+
+  belongs_to :user
+
+  validates_uniqueness_of :uid, scope: :provider
+  validates_uniqueness_of :provider, scope: :provider
+
+  scope :facebook, where(provider: 'facebook')
+end
