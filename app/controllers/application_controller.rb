1d64354b93865e5044b064e62625e24d6a121b84
Jesse Cravens <jesse.cravens@gmail.com>
2012-04-29 19:30:19 +0500

    Subdomain and token authenticable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
   before_filter :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+
+  def after_sign_in_path_for(resource)
+    root_url(subdomain: resource.company.subdomain)
   end
 end
