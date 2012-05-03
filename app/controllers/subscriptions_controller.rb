26c12d54c65839ff92011b94a3f720b899423d22
Shirjeel Alam <shirjeel20@gmail.com>
2012-05-03 15:54:20 +0500

    Ability to change plan (Upgrade + Downgrade)

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,8 +1,16 @@
 class SubscriptionsController < ApplicationController
   def edit
     @subscription = Subscription.find(params[:id])
+    @plans = Plan.find(Plan.all.collect { |plan| plan.id unless plan == @subscription.plan }.compact )
   end
 
   def update
+    @subscription = Subscription.find(params[:id])
+    @company = @subscription.company
+    @company.cancel_subscription
+
+    @company.create_subscription(Plan.find(params[:subscription][:plan]), params[:subscription][:stripe_card_token])
+
+    redirect_to current_user, notice: 'Plan successfully changed'
   end
 end
