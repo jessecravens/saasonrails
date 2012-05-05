d94a25c38b4aca1b5f1f9f42f97c00a3791dbe28
Jesse Cravens <jesse.cravens@gmail.com>
2012-05-06 01:19:34 +0500

    Added cancan abilities. Cleaned up the index page for users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,8 @@ MicrobizRails32MongoDevise::Application.configure do
   #For MailCatcher
   config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
   # change to false to prevent email from being sent during development
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger
